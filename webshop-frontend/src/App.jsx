0adc1e92ed2a031ca56b5e4767973918e62949d3
Frank <franko.pavlic@gmail.com>
2025-10-26 11:50:23 +0100

    Fix case-sensitive import for Home page

--- webshop-frontend/src/App.jsx
+++ webshop-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import { CartProvider } from "./context/CartContext";
