fc7c70dee7075fa7b71be0b03863f1fb3b6a6488
Frank <franko.pavlic@gmail.com>
2025-12-17 23:19:12 +0100

    fix

--- webshop-backend/src/routes/stripeWebhooks.js
+++ webshop-backend/src/routes/stripeWebhooks.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const Stripe = require("stripe");
 const { PrismaClient } = require("@prisma/client");
-const axios = require("axios");
+import axios from "axios";
 
 const prisma = new PrismaClient();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
